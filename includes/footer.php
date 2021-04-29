780e485ac5003bde90e2826453dd728ca01811e2
SK SOHAIL <83121133+SOHAIL-SOURCE@users.noreply.github.com>
2021-04-30 00:30:17 +0530

    Added the .include file

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+
+
+<footer>
+	<div class="container">
+            <center>
+		Copyright <span class="glyphicon glyphicon-copyright-mark"></span> LifeStyle Stores.
+		All Rights Reserved | Contact Us: +91 90000 00000
+            </center>
+        </div>
+</footer>
